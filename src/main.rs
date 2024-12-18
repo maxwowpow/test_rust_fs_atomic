69358d75651c731c46989b80d10c0e32e869503d
Max <max@users.noreply.github.commax@internal>
2024-12-18 01:31:20 +0100

    write_all as in zed

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use anyhow::Result;
+use env_logger::Builder;
+use log::info;
+use log::LevelFilter;
 use std::env;
 use std::fs;
 use std::io::Write;
 use std::path::Path;
 use tempfile::NamedTempFile;
-use anyhow::Result;
-use log::info;
-use env_logger::Builder;
-use log::LevelFilter;
 
 fn prepare_test_data() {
     let app_data_dir = "app_data";
@@ -21,8 +21,10 @@ fn prepare_test_data() {
 
     // Create placeholder file for getting parent folder
     if !Path::new(input_file).exists() {
-        let mut file = fs::File::create(input_file).expect("Failed to create placeholder_for_getting_parent_folder.txt");
-        writeln!(file, "placeholder_for_getting_parent_folder").expect("Failed to write to placeholder_for_getting_parent_folder.txt");
+        let mut file = fs::File::create(input_file)
+            .expect("Failed to create placeholder_for_getting_parent_folder.txt");
+        writeln!(file, "placeholder_for_getting_parent_folder")
+            .expect("Failed to write to placeholder_for_getting_parent_folder.txt");
     }
 
     // Create target directory
@@ -37,9 +39,7 @@ fn prepare_test_data() {
 }
 
 fn main() -> Result<()> {
-    Builder::new()
-        .filter_level(LevelFilter::Info)
-        .init();
+    Builder::new().filter_level(LevelFilter::Info).init();
 
     prepare_test_data();
 
@@ -47,24 +47,27 @@ fn main() -> Result<()> {
     info!("Arguments: {:?}", args);
 
     if args.len() < 3 {
-        eprintln!(
-            "Usage: {} <existing_file_path> <output_file_path>",
-            args[0]
-        );
+        eprintln!("Usage: {} <existing_file_path> <output_file_path>", args[0]);
         std::process::exit(1);
     }
 
     let a = &args[1];
     let b = &args[2];
 
-    info!("Using '{}' to determine the parent directory for the temporary file.", a);
+    info!(
+        "Using '{}' to determine the parent directory for the temporary file.",
+        a
+    );
     let parent = Path::new(a).parent().unwrap_or_else(|| Path::new("."));
     info!("Parent directory for temporary file: {:?}", parent);
 
     let mut tmp = NamedTempFile::new_in(parent)?;
-    info!("Temporary file created at: {:?}", tmp.path());
 
-    write!(tmp, "")?;
+    info!("Temporary file created at: {:?}", tmp.path());
+    // todo linux test (as in zed)
+    tmp.write_all("".as_bytes())?;
+    // works
+    // write!(tmp, "")?;
     info!("Trying to persist to path: {}", b);
     tmp.persist(b)?;
 
@@ -72,4 +75,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
